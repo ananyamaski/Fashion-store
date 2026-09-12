bfacb037b3ffc27386c00609072f9626a9424096
Ananya <ananyamaski@gmail.com>
2026-09-12 12:05:09 +0530

    Secure production settings

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w8t&p!k^#0gd-(1)u&!!@3@u@a7*d9f!l=os#s3y!6-@qglj*)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'fashion-store-u3xn.onrender.com',
